970f68afd36a5feca8616ae66f20c33502bef2bc
thefierytortoise <austindobrusky@gmail.com>
2020-02-18 15:23:39 -0600

    change theme

--- js/master.js
+++ js/master.js
@@ -1,21 +1,30 @@
-function navbarChange() {
-  let scroll = $(window).scrollTop();
-  if(screenWidth > medium) {
-    if(scroll > 0) {
-      $('.nav').css('backgroundColor', 'red');
+function changeTheme() { //Changes color of navbar on scroll down
+  if(screenWidth > large) {
+    if(scroll >= $('.nav').height()) {
+      $('.nav').css('backgroundColor', '#0e1221');
+      $('.nav-items').css('backgroundColor', 'transparent');
     } else {
-      $('.nav').css('backgroundColor', 'transparent');
+      $('.nav').css('backgroundColor', 'rgba(14,18,33,0.56)');
+      $('.nav-items').css('backgroundColor', 'transparent');
+    }
+  } else if(screenWidth > medium && screenWidth < large) {
+    if(scroll >= ($('.nav').height() + $('.nav-items').height())) {
+      $('.nav').css('backgroundColor', '#0e1221');
+      $('.nav-items').css('backgroundColor', '#0e1221');
+    } else {
+      $('.nav').css('backgroundColor', 'rgba(14,18,33,0.56)');
+      $('.nav-items').css('backgroundColor', 'rgba(14,18,33,0.56)');
     }
   } else {
-    $('.nav').css('backgroundColor', '#ffcb82');
+    $('.nav').css('backgroundColor', '#0e1221');
+    $('.nav-items').css('backgroundColor', '#0e1221');
   }
-  console.log(scroll);
-}
+};
 
 $(document).scroll(function() {
-  navbarChange();
+  changeTheme();
 });
 
 $(document).ready(function() {
-  navbarChange();
+  changeTheme();
 });
